25ab4ed506cb22057bd190e62e8946bd03760534
Huynhdavid888 <huynhdavid1245@gmail.com>
2021-07-05 19:18:44 -0700

    initial commit

---
+++ LINQexercise2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LINQexercise2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var videoGames = new List<string> { "God of War", "Halo", "Sea of Thieves", "Pokemon", "Apex Legends", "League of Legends" };
+            var LengthNames = videoGames.Where(name => name.Length > 1).OrderByDescending(name => name.Length);
+            foreach(var name in LengthNames)
+            {
+                Console.WriteLine(name);
+            }
+
+
+
+        }
+    }
+}
